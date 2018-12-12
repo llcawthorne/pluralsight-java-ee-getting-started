f8da30de31fe78ed2fcc2cd1e563e5f9b3f01ca3
Lewis Cawthorne <LLC@acm.org>
2018-12-11 21:14:05 -0500

    Enabled basic navigation

--- bookstore-front/src/app/book-detail/book-detail.component.ts
+++ bookstore-front/src/app/book-detail/book-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'bs-book-detail',
@@ -16,9 +17,13 @@ export class BookDetailComponent implements OnInit {
     Language: "English"
   }
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  delete() {
+    // invoke our REST API
+    this.router.navigate(['/book-list']);
+  }
 }
